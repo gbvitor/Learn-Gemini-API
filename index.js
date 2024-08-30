087b03984cb104d17bca57b9ac009f8ef6178886
Vitor Gabriel <sgvitor@outlook.com.br>
2024-08-30 12:00:04 -0300

    fix: internal error gemini api version

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
     // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const prompt = await fazerPergunta("Me fale uma pergunta: ");
+    const prompt = await fazerPergunta("Me faça uma pergunta: ");
+
+    // Count the number of tokens in the prompt
+    const countResult = await model.countTokens(prompt);
+    const generateResult = await model.generateContent(prompt);
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     console.log(text);
-    console.log(prompt);
+    console.log(`Tokens: ${countResult.totalTokens}`);
+    console.log(generateResult.response.usageMetadata);
 }
 
 run();
